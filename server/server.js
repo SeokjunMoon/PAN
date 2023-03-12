0bd53a0546e8be31cf3bcf90738fbc052d2f8bfe
SeokjunMoon <seokjun0915@icloud.com>
2023-03-12 16:05:42 +0900

    불필요한 데이터 추가 코드 삭제

--- server/server.js
+++ server/server.js
@@ -17,28 +17,10 @@ const {
 } = require('./models');
 sequelize.query('SET NAMES utf8;');
 
-app.post('/add/data', (req, res) => {
-    console.log(req.body);
-
-    Cse.create({
-        index: req.body.index,
-        title: req.body.title,
-        link: req.body.link,
-        date: req.body.date
-    })
-    .then(result => {
-        res.send(result);
-    })
-    .catch(err => {
-        console.log(err);
-        throw err;
-    })
-});
 
 app.get('/get/data', (req, res) => {
     Cse.findAll()
     .then(result => {
-        console.log(result);
         res.send(result);
     })
     .catch(err => {
