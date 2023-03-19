15efec1b7d4a686c1bc5d1438fd6689ff5ae12f4
SeokjunMoon <seokjun0915@icloud.com>
2023-03-19 14:57:32 +0900

    불필요한 파일 삭제

--- server/python/main-cse.py
+++
@@ -1,6 +0,0 @@
-import cse_parser
-
-
-cse = cse_parser.CseParser()
-cse.parseData()
-cse.saveData()
--- server/python/main-pnu.py
+++
@@ -1,5 +0,0 @@
-import pnu_parser
-
-pnu = pnu_parser.PnuParser()
-pnu.parseData()
-pnu.saveData()
